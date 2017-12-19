ae9b03ac2ea077ce260d74b6b67395886b2121ec
Ciao Huang <riverciao@gmail.com>
2017-12-19 17:36:44 +0800

    add address to search result

--- FollowMe/LocationSearchTableViewController.swift
+++ FollowMe/LocationSearchTableViewController.swift
@@ -32,8 +32,32 @@ class LocationSearchTableViewController: UITableViewController {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
         let selectedItem = matchingItems[indexPath.row].placemark
         cell.textLabel?.text = selectedItem.name
-        cell.detailTextLabel?.text = ""
+        cell.detailTextLabel?.text = parseAddress(selectedItem: selectedItem)
         return cell
+    }
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil && selectedItem.thoroughfare != nil) ? " " : ""
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) && (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil && selectedItem.administrativeArea != nil) ? " " : ""
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        return addressLine
     }
 
 }
@@ -55,6 +79,7 @@ extension LocationSearchTableViewController : UISearchResultsUpdating {
             }
             
             guard let response = response else { return }
+            print("OOO:\(response)")
             self.matchingItems = response.mapItems
             self.tableView.reloadData()
         }
