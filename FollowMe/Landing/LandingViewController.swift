b4433418d69f6e7b4e2cfd09a54a1540bd060845
Ciao Huang <riverciao@gmail.com>
2017-12-28 12:54:26 +0800

    delete unnesassary code

---
+++ FollowMe/Landing/LandingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LandingViewController.swift
+//  FollowMe
+//
+//  Created by riverciao on 2017/12/28.
+//  Copyright © 2017年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
