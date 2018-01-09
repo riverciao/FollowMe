8f37ad155d7287ecde7c73663a5f692d35146a2a
Ciao Huang <riverciao@gmail.com>
2018-01-09 10:38:22 +0800

    add a sub class of UIView: FadingOutView

---
+++ FollowMe/UIView/FadingOutView.swift
@@ -0,0 +1,85 @@
+//
+//  FadingOutView.swift
+//  FollowMe
+//
+//  Created by riverciao on 2018/1/9.
+//  Copyright © 2018年 riverciao. All rights reserved.
+//
+
+import UIKit
+
+class FadingOutView: UIView {
+
+    /// starting view alpha (default: 0.5)
+    open var startingAlpha: CGFloat = 0.5
+    /// view showing time (default: 5)
+    open var showingTime: TimeInterval = 5
+    /// view fading out time (default: 0.1)
+    open var fadingTimeInterval: TimeInterval = 0.1
+    /// view fading out alpha per viewFadingTimeInterval (default: 0.1)
+    open var viewFadingOutAlpha: CGFloat = 0.1
+    
+    /// fadeOut timer
+    fileprivate var fadeOutTimer = Timer()
+    
+    
+    /**
+     Init view
+     
+     - parameter frame: view frame
+     
+     - returns: view
+     */
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        var frame = self.bounds
+        frame.origin.y = frame.size.height
+        frame.size.height = 0
+        self.backgroundColor = UIColor.clear
+    }
+    
+    /**
+     Init view with wave color
+     
+     - parameter frame: view frame
+     - parameter color: real wave color
+     
+     - returns: view
+     */
+    public convenience init(frame: CGRect, startingAlpha: CGFloat, showingTime: TimeInterval) {
+        self.init(frame: frame)
+        
+        self.startingAlpha = startingAlpha
+        self.showingTime = showingTime
+        
+        setupTimer()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    private func setupTimer() {
+        Timer.scheduledTimer(timeInterval: showingTime, target: self, selector: #selector(callFadeOutTimer), userInfo: nil, repeats: false)
+    }
+
+    @objc private func callFadeOutTimer() {
+        fadeOutTimer = Timer.scheduledTimer(timeInterval: fadingTimeInterval, target: self, selector: #selector(fadeOut), userInfo: nil, repeats: true)
+    }
+
+    @objc private func fadeOut() {
+
+        if startingAlpha > 0 {
+            startingAlpha -= viewFadingOutAlpha
+            self.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: startingAlpha)
+        } else if startingAlpha <= 0 {
+            self.isHidden = true
+            fadeOutTimer.invalidate()
+        }
+    }
+    
+
+}
